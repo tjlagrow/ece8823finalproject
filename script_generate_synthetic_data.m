35dc31b9a025405d88030bff4a9605ff6c443a46
TJ LaGrow <tlagrow@uoregon.edu>
2019-05-02 04:41:58 -0400

    cleaning

--- script_generate_synthetic_data.m
+++
@@ -1,8 +0,0 @@
-%{
-script for generating the synthetic data used in this project
-%}
-
-[toysylvania, toysylvania_information] = generate_toysylvania(1,1);
-[newflormedium, newflormedium_information] = generate_newflormedium(1,1);
-[smallerado, smallerado_information] = generate_smallerado(1,1);
-[largerado, largerado_information] = generate_largerado(1,1);
